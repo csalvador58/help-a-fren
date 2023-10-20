b4dce2d54f733d3d07e9f998cf28788cf57f85a8
Randee Layosa <randeelayosa@gmail.com>
2023-10-20 05:04:08 -0700

    build out results page

--- app/packages/nextjs/pages/example-ui.tsx
+++ app/packages/nextjs/pages/example-ui.tsx
@@ -251,7 +251,6 @@ const ExampleUI: NextPage = () => {
           {/* Proposer */}
           <div className="card w-full bg-base-100 shadow-xl">
             <div className="card-body haf-card-body gap-0 p-0 grid md:grid-cols-[35%_65%]">
-              
               <div className="haf-purple grid gap-5 p-md place-content-center bg-primary text-primary-content rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none md:rounded-bl-2xl image-full">
                 <figure><img src="./haf-logo-drop-shadow.svg" className="w-6/12 mx-auto max-w-lg" /></figure>
                 <div className="text-lg">
@@ -267,7 +266,6 @@ const ExampleUI: NextPage = () => {
                   <a href="https://www.google.com" target="_blank"><img src="./assets/icon-open-browser.svg" className="w-4 ml-sm inline-flex" /></a>
                 </div>
               </div>
-
               <div className="grid gap-5 p-md">
                 <div className="card-header-wrapper place-self-center">
                   <div className="card-header max-w-lg">
@@ -361,125 +359,177 @@ const ExampleUI: NextPage = () => {
 
           {/* Voter */}
           <div className="card w-full bg-base-100 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">Vote</h2>
-              <p>Claims a vote NFT, only one token can be claimed</p>
-              <p>Indicate if you are for, against, or abstain from the proposal request. Be sure to submit each vote.</p>
-              <div className="form-control">
-                <h3>Proposal 1</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend non sapien a condimentum. Nunc imperdiet lorem non massa lobortis dignissim. Nam at nibh iaculis, sagittis sem non, scelerisque tortor. Phasellus dictum lorem at felis fringilla efficitur sit amet a ante. Pellentesque at molestie velit, eu finibus sem.</p>
-                <div className="w-full max-w-xs">
-                  <label className="label">
-                    <span className="label-text">Decision</span>
-                  </label>
-                  <select className="select select-bordered">
-                    <option disabled selected>Select</option>
-                    <option>For</option>
-                    <option>Against</option>
-                    <option>Abstain</option>
-                  </select>
+            <div className="card-body haf-card-body gap-0 p-0 grid md:grid-cols-[35%_65%]">
+              <div className="haf-purple grid gap-5 p-md place-content-center bg-primary text-primary-content rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none md:rounded-bl-2xl image-full">
+                <figure><img src="./haf-logo-drop-shadow.svg" className="w-6/12 mx-auto max-w-lg" /></figure>
+                <div className="text-lg">
+                  <p className=" inline-flex">Location</p>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm inline-flex">USA</span>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm  inline-flex">Lahaina</span>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm  inline-flex">Maui</span>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm  inline-flex">96761</span>
                 </div>
-                <h3>Asking</h3>
-                <p>$XX,XXX</p>
-                <div className="card-actions justify-end">
-                  <button
-                    className="btn btn-accent"
-                  >
-                        Submit
-                  </button>
+                <div className="text-lg">
+                  <p className="inline-flex">Wallet</p>
+                  <div className="badge badge-md badge-accent badge-outline ml-sm inline-flex break-all">0x999999999abcdefghijklmnop</div>
+                  <a href="https://www.google.com" target="_blank"><img src="./assets/icon-open-browser.svg" className="w-4 ml-sm inline-flex" /></a>
                 </div>
               </div>
-              <div className="form-control">
-                <h3>Proposal 2</h3>
-                <p>Donec risus nunc, gravida quis congue et, convallis ut ipsum. In scelerisque dictum diam sit amet eleifend. Pellentesque non ipsum ac diam cursus aliquet non id est.</p>
-                <div className="w-full max-w-xs">
-                  <label className="label">
-                    <span className="label-text">Decision</span>
-                  </label>
-                  <select className="select select-bordered">
-                    <option disabled selected>Select</option>
-                    <option>For</option>
-                    <option>Against</option>
-                    <option>Abstain</option>
-                  </select>
+              <div className="grid gap-5 p-md">
+                <div className="card-header-wrapper place-self-center">
+                  <div className="card-header max-w-lg">
+                    <h2 className="card-title justify-center">Vote</h2>
+                    <p className="text-center max-w-lg justify-center">You must be a verifiable resident or business owner in the aforementioned area in order to successfully cast votes. Be sure to submit each proposal vote in order for it to count.</p>
+                  </div>
                 </div>
-                <h3>Asking</h3>
-                <p>$XX,XXX</p>
-                <div className="card-actions justify-end">
-                  <button
-                    className="btn btn-accent"
-                  >
-                        Submit
-                  </button>
+                <div className="grid lg:grid-cols-2 grid-flow-row gap-7 p-6 nested-card-wrapper">
+                  <div className="form-control nested-card">
+                    <div className="stat my-0 place-items-end">
+                      <div className="stat-value">$20,000</div>
+                      <div className="stat-desc">Asking</div>
+                    </div>
+                    <p className="small-text">Proposal 1</p>
+                    <h3>Title</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend non sapien a condimentum. Nunc imperdiet lorem non massa lobortis dignissim. Nam at nibh iaculis, sagittis sem non, scelerisque tortor. Phasellus dictum lorem at felis fringilla efficitur sit amet a ante. Pellentesque at molestie velit, eu finibus sem.</p>
+                    <div className="w-full flex flex-row justify-between">
+                      <div className="max-w-xs">
+                        <select className="select select-bordered">
+                          <option disabled selected>Select</option>
+                          <option>For</option>
+                          <option>Against</option>
+                          <option>Abstain</option>
+                        </select>
+                      </div>
+                      <div className="card-actions">
+                        <button
+                          className="btn btn-accent"
+                        >
+                              Submit
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-control nested-card">
+                    <div className="stat my-0 place-items-end">
+                      <div className="stat-value">$50,000</div>
+                      <div className="stat-desc">Asking</div>
+                    </div>
+                    <p className="small-text">Proposal 2</p>
+                    <h3>Title</h3>
+                    <p>Donec risus nunc, gravida quis congue et, convallis ut ipsum. In scelerisque dictum diam sit amet eleifend. Pellentesque non ipsum ac diam cursus aliquet non id est.</p>
+                    <div className="w-full flex flex-row justify-between">
+                      <div className="max-w-xs">
+                        <select className="select select-bordered">
+                          <option disabled selected>Select</option>
+                          <option>For</option>
+                          <option>Against</option>
+                          <option>Abstain</option>
+                        </select>
+                      </div>
+                      <div className="card-actions">
+                        <button
+                          className="btn btn-accent"
+                        >
+                              Submit
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-control nested-card">
+                    <div className="stat my-0 place-items-end">
+                      <div className="stat-value">Unlimited</div>
+                      <div className="stat-desc">Asking</div>
+                    </div>
+                    <p className="small-text">Proposal 3</p>
+                    <h3>Title</h3>
+                    <p>Sed hendrerit porttitor ex a pulvinar. Pellentesque at accumsan nisi, eu commodo nibh. In ultrices, augue at bibendum mollis, neque eros aliquet est, vel ullamcorper ex neque ac magna.</p>
+                    <div className="w-full flex flex-row justify-between">
+                      <div className="max-w-xs">
+                        <select className="select select-bordered">
+                          <option disabled selected>Select</option>
+                          <option>For</option>
+                          <option>Against</option>
+                          <option>Abstain</option>
+                        </select>
+                      </div>
+                      <div className="card-actions">
+                        <button
+                          className="btn btn-accent"
+                        >
+                              Submit
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="form-control">
-                <h3>Proposal 3</h3>
-                <p>Sed hendrerit porttitor ex a pulvinar. Pellentesque at accumsan nisi, eu commodo nibh. In ultrices, augue at bibendum mollis, neque eros aliquet est, vel ullamcorper ex neque ac magna.</p>
-                <div className="w-full max-w-xs">
-                  <label className="label">
-                    <span className="label-text">Decision</span>
-                  </label>
-                  <select className="select select-bordered">
-                    <option disabled selected>Select</option>
-                    <option>For</option>
-                    <option>Against</option>
-                    <option>Abstain</option>
-                  </select>
-                </div>
-                <h3>Asking</h3>
-                <p>Unlimited, On-Going</p>
-                <div className="card-actions justify-end">
-                  <button
-                    className="btn btn-accent"
-                  >
-                        Submit
-                  </button>
-                </div>
-              </div>
-              <div className="card-actions justify-end">
-                <button
-                  className="btn btn-accent"
-                  onClick={handleNFTTokenClaim}
-                  disabled={isClaimVoteTokenLoading || isDelegateVoteTokenLoading}
-                >
-                  {isClaimVoteTokenLoading || isDelegateVoteTokenLoading ? (
-                    <span className="loading loading-spinner loading-sm"></span>
-                  ) : (
-                    <>
-                      Claim <ArrowSmallRightIcon className="w-3 h-3 mt-0.5" />
-                    </>
-                  )}
-                </button>
               </div>
             </div>
           </div>
-
+          
           {/* Results */}
           <div className="card w-full bg-base-100 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">Results</h2>
-              <p>The following information indicates the status and result of each proposal that has been submitted thus far.</p>
-              <div className="card-actions justify-end">
-                <p>Donations to date: <span className="ml-5">$XXX,XXX</span></p>
+            <div className="card-body haf-card-body gap-0 p-0 grid md:grid-cols-[35%_65%]">
+              <div className="haf-purple grid gap-5 p-md place-content-center bg-primary text-primary-content rounded-tl-2xl rounded-tr-2xl md:rounded-tr-none md:rounded-bl-2xl image-full">
+                <figure><img src="./haf-logo-drop-shadow.svg" className="w-6/12 mx-auto max-w-lg" /></figure>
+                <div className="text-lg">
+                  <p className=" inline-flex">Location</p>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm inline-flex">USA</span>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm  inline-flex">Lahaina</span>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm  inline-flex">Maui</span>
+                  <span className="badge badge-md badge-accent badge-outline ml-sm  inline-flex">96761</span>
+                </div>
+                <div className="text-lg">
+                  <p className="inline-flex">Wallet</p>
+                  <div className="badge badge-md badge-accent badge-outline ml-sm inline-flex break-all">0x999999999abcdefghijklmnop</div>
+                  <a href="https://www.google.com" target="_blank"><img src="./assets/icon-open-browser.svg" className="w-4 ml-sm inline-flex" /></a>
+                </div>
               </div>
-              <div>
-                <h3>Proposal 1</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend non sapien a condimentum. Nunc imperdiet lorem non massa lobortis dignissim. Nam at nibh iaculis, sagittis sem non, scelerisque tortor. Phasellus dictum lorem at felis fringilla efficitur sit amet a ante. Pellentesque at molestie velit, eu finibus sem.</p>
-                <h3>Status</h3>
-                <a href="#">Approved and Sent</a>
-              </div>
-              <div>
-                <h3>Proposal 2</h3>
-                <p>Donec risus nunc, gravida quis congue et, convallis ut ipsum. In scelerisque dictum diam sit amet eleifend. Pellentesque non ipsum ac diam cursus aliquet non id est.</p>
-                <h3>Status</h3>
-                <p>Active</p>
-              </div>
-              <div>
-                <h3>Proposal 3</h3>
-                <p>Sed hendrerit porttitor ex a pulvinar. Pellentesque at accumsan nisi, eu commodo nibh. In ultrices, augue at bibendum mollis, neque eros aliquet est, vel ullamcorper ex neque ac magna.</p>
-                <h3>Status</h3>
-                <p>Active</p>
+              <div className="grid gap-5 p-md">
+                <div className="card-header-wrapper place-self-center">
+                  <div className="card-header max-w-lg">
+                    <h2 className="card-title justify-center">Results</h2>
+                    <p className="text-center max-w-lg justify-center">You must be a verifiable resident or business owner in the aforementioned area in order to successfully cast votes. Be sure to submit each proposal vote in order for it to count.</p>
+                  </div>
+                </div>
+                <div className="grid lg:grid-cols-2 grid-flow-row gap-7 p-6 nested-card-wrapper">
+                  <div className="form-control nested-card bg-light">
+                    <div className="stat my-0 place-items-end">
+                      <div className="stat-value">$20,000</div>
+                      <div className="stat-desc">Asking</div>
+                    </div>
+                    <p className="small-text">Proposal 1</p>
+                    <h3>Title</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend non sapien a condimentum. Nunc imperdiet lorem non massa lobortis dignissim. Nam at nibh iaculis, sagittis sem non, scelerisque tortor. Phasellus dictum lorem at felis fringilla efficitur sit amet a ante. Pellentesque at molestie velit, eu finibus sem.</p>
+                    <div className="w-full text-center">
+                      <p className="inline-flex">Approved and Sent</p>
+                      <a href="https://www.google.com" target="_blank"><img src="./assets/icon-open-browser.svg" className="w-4 ml-sm inline-flex" /></a>
+                    </div>
+                  </div>
+                  <div className="form-control nested-card">
+                    <div className="stat my-0 place-items-end">
+                      <div className="stat-value">$50,000</div>
+                      <div className="stat-desc">Asking</div>
+                    </div>
+                    <p className="small-text">Proposal 2</p>
+                    <h3>Title</h3>
+                    <p>Donec risus nunc, gravida quis congue et, convallis ut ipsum. In scelerisque dictum diam sit amet eleifend. Pellentesque non ipsum ac diam cursus aliquet non id est.</p>
+                    <div className="w-full flex flex-row text-center">
+                      <p>Active</p>
+                    </div>
+                  </div>
+                  <div className="form-control nested-card">
+                    <div className="stat my-0 place-items-end">
+                      <div className="stat-value">Unlimited</div>
+                      <div className="stat-desc">Asking</div>
+                    </div>
+                    <p className="small-text">Proposal 3</p>
+                    <h3>Title</h3>
+                    <p>Sed hendrerit porttitor ex a pulvinar. Pellentesque at accumsan nisi, eu commodo nibh. In ultrices, augue at bibendum mollis, neque eros aliquet est, vel ullamcorper ex neque ac magna.</p>
+                    <div className="w-full flex flex-row text-center">
+                      <p>Active</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
